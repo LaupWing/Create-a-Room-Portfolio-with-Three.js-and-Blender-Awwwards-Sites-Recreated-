720ac40965c7bd21e426f7f78d1031a0ff03bc4f
LaupWing <loc_strict@hotmail.com>
2022-08-29 12:11:25 +0200

    update

--- room_website/Experience/Utils/Renderer.js
+++ room_website/Experience/Utils/Renderer.js
@@ -17,6 +17,15 @@ export default class Renderer {
          canvas: this.canvas,
          antialias: true
       })
+
+      this.renderer.physicallyCorrectLights = true
+      this.renderer.outputEncoding = THREE.sRGBEncoding
+      this.renderer.toneMapping = THREE.CineonToneMapping
+      this.renderer.toneMappingExposure = 1.75
+      this.renderer.shadowMap.enabled = true
+      this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
+      this.renderer.setSize(this.sizes.width, this.sizes.height)
+      this.renderer.setPixelRatio(this.sizes.pixelRatio)
    }
 
    resize(){
