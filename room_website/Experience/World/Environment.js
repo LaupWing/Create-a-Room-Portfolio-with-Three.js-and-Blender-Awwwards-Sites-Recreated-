194ba73dfe0ae753ed7847c6a5154940c8137cd6
LaupWing <loc_strict@hotmail.com>
2022-09-01 08:39:29 +0200

    hehe

--- room_website/Experience/World/Environment.js
+++ room_website/Experience/World/Environment.js
@@ -14,10 +14,13 @@ export default class Environment {
       this.sunlight = new THREE.DirectionalLight("#ffffff", 3)
       this.sunlight.castShadow = true
       this.sunlight.shadow.camera.far = 20
-      this.sunlight.shadow.mapSize.set(1024, 1024)
+      this.sunlight.shadow.mapSize.set(2048, 2048)
       this.sunlight.shadow.normalBias = 0.05
       this.sunlight.position.set(1.5, 7, 3)
       this.scene.add(this.sunlight)
+
+      this.ambientLight = new THREE.AmbientLight("#ffffff", 1)
+      this.scene.add(this.ambientLight)
    }
 
    resize(){
